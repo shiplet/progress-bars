90f29e51302efba4dd3034785506463fa0f46933
Ship <mnshiplet@gmail.com>
2020-02-24 20:55:14 -0700

    rust speed optimized

--- rust/ansi-multiple/src/main.rs
+++ rust/ansi-multiple/src/main.rs
@@ -1,5 +1,6 @@
+use std::io::{Write, BufWriter, stdout};
 use rand::{thread_rng, Rng};
-use std::time::{SystemTime};
+use std::time::SystemTime;
 use terminal_size::{Width, Height, terminal_size};
 
 const COUNT: usize = 30;
@@ -7,7 +8,7 @@ const RANGE: i64 = 100;
 
 fn outstr(index: &i32, total: i64, scale: f64) -> String {
     let width: usize = (100.0 * scale).round() as usize;
-    let current: usize= (((*index as f64 / total as f64) * 100.0) * scale).round() as usize;
+    let current: usize = (((*index as f64 / total as f64) * 100.0) * scale).round() as usize;
     let percentage: usize = ((*index as f64 / total as f64) * 100.0).round() as usize;
     format!("[{}>{}] {}%", "=".repeat(current), " ".repeat(width - current), percentage)
 }
@@ -46,24 +47,59 @@ fn main() {
         term_width = w;
     }
 
-    print!("{}", "\n".repeat(COUNT));
-    while !check_complete(&all_progress_bars) {
-        let incomplete = get_incomplete_indexes(&all_progress_bars);
-        let rnd_index = rng.gen_range(0, incomplete.len());
-        all_progress_bars[incomplete[rnd_index] as usize] += 1;
-        print!("\u{001b}[{}D", term_width);
-        print!("\u{001b}[{}A", COUNT);
+    let mut count: usize = 0;
 
-        for bar in all_progress_bars.iter() {
-            print!("{}\n", outstr(bar, RANGE, 1.0));
+    let mut incomplete_indexes_timer: Vec<u128> = vec![];
+    let mut random_gen_number: Vec<u128> = vec![];
+    let mut print_time_count: Vec<u128> = vec![];
+
+    let std = stdout();
+    let lock = std.lock();
+    let mut w = BufWriter::new(lock);
+
+    write!(w, "{}", "\n".repeat(COUNT)).unwrap();
+    while !check_complete(&all_progress_bars) {
+        count += 1;
+        let incomplete_start = SystemTime::now();
+        let incomplete = get_incomplete_indexes(&all_progress_bars);
+        match incomplete_start.elapsed() {
+            Ok(elapsed) => incomplete_indexes_timer.push(elapsed.as_nanos()),
+            _ => ()
         }
+
+        let rnd_start = SystemTime::now();
+        let rnd_index = rng.gen_range(0, incomplete.len());
+        match rnd_start.elapsed() {
+            Ok(elapsed) => random_gen_number.push(elapsed.as_nanos()),
+            _ => ()
+        }
+
+        all_progress_bars[incomplete[rnd_index] as usize] += 1;
+        write!(w, "\u{001b}[{}D", term_width).unwrap();
+        write!(w, "\u{001b}[{}A", COUNT).unwrap();
+
+        let print_start = SystemTime::now();
+        for bar in all_progress_bars.iter() {
+            write!(w, "{}\n", outstr(bar, RANGE, 1.0)).unwrap();
+        }
+        match print_start.elapsed() {
+            Ok(elapsed) => print_time_count.push(elapsed.as_nanos()),
+            _ => ()
+        }
+
     }
     match start.elapsed() {
         Ok(elapsed) => {
-            println!("Time: {}s", elapsed.as_secs_f32());
+            write!(w, "Time: {}s\n", elapsed.as_secs_f32()).unwrap();
+            write!(w, "Iterations: {}\n", count).unwrap();
+            write!(w, "Time per op: {}\n", elapsed.as_secs_f32() / count as f32).unwrap();
+            let mut total: u128 = 0;
+            for (_i, &val) in print_time_count.iter().enumerate() {
+                total += val;
+            }
         }
         Err(e) => {
-            println!("Error: {:?}", e);
+            write!(w, "Error: {:?}\n", e).unwrap();
         }
     }
 }
